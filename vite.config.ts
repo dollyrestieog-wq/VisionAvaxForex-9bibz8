e519b0a5a3b6364678478266b4ecba848638e923
dollyrestieog-wq <dollyrestieog@gmail.com>
2026-07-17 19:04:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/VisionAvaxForex-9bibz8/",
   plugins: [react()],
   resolve: {
     alias: {
